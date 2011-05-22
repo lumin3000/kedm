50cf6733820e0fa8294b3e7e36cecb3d3a151cd9
fancyoung <siyang1982@msn.com>
2011-05-22 12:41:57 +0800

    -

--- main.rb
+++ main.rb
@@ -13,7 +13,7 @@ confs = [
          {body: get_body_html(2), file: "qq_f_90_01"},
          {body: get_body_html(3), file: "qq_m_80_01"},
          {body: get_body_html(4), file: "qq_f_90_01"},
-         {body: get_body_html(5), file: "qq_f_80_02"},
+         {body: get_body_html(1), file: "qq_f_80_01_l"},
          {body: get_body_html(1), file: "m_m_90_01"},
          {body: get_body_html(2), file: "m_f_90_01"},
          {body: get_body_html(3), file: "m_m_80_01"},
@@ -37,14 +37,14 @@ file.each_line do |line|
  if(line =~ /.+@.+\..+/ && (line =~ /-/) != 0)
  send_mail(email_r, line, subject_r(line.sub(/@.+/, '')), body_txt, body_html)
  log i.to_s + " : " + ARGV.to_s + " : " + line, log_name
- sleep 8
- if i%600 == 0
+ sleep 1
+ if i%30000 == 0
    if file_name =~ /qq/
      send_mail(email_r, "25788518@qq.com", subject_r(file_name + line.sub(/@.+/, '')) , body_txt, body_html)
    else
      send_mail(email_r, "siyang1982@gmail.com", subject_r(file_name + line.sub(/@.+/, '')), body_txt, body_html)
    end
-   sleep 30
+   sleep 10
  end
  i += 1
  end
